e54d438b01039d859b67429a6eb2d8ff920b4c2a
Leo Mal <mrlevopold@gmail.com>
2014-10-27 23:16:23 +0400

    Vk auth work Need to add cookie

--- php/auth.php
+++ php/auth.php
@@ -11,19 +11,20 @@ $fb_name='';
 //print_r($result[0]+' '+);
 
  $conn = mysql_connect("u389193.mysql.masterhost.ru", "u389193", "me24..NCIng") or die(mysqli_error("error connecting to database"));
- //mysql_query("SET NAMES 'utf8'");
- //mysql_query("SET CHARACTER SET 'utf8");
+ mysql_query("SET NAMES 'utf8'");
+ mysql_query("SET CHARACTER SET 'utf8'");
+
  	if (!$conn) {
  		die('Connection fail: ' . mysql_error());
  	}
   mysql_select_db('u389193') or die(mysql_error ());
-  $res= mysql_query("SELECT id from acc where vk_uid='$id'");
+  $res= mysql_query("SELECT id from acc where vk_name='$login'");
   	if (!$res) {
   		$message  = 'Wrong sql: ' . mysql_error() . "\n";
   		$message .= 'sql: ' . $res;
   		die($message);
   	}
-  	if ($id=mysql_result($res,0)==0)
+  	if ($idBd=mysql_result($res,0)==0)
     	{
     		 $sql = "INSERT INTO acc (vk_name, vk_uid, vk_photo, vk_smallPhoto, fb_name) VALUES ('$login', '$id', '$photo', '$smallPhoto', '$fb_name')";
               $retVal = mysql_query( $sql, $conn);
